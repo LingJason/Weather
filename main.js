878a9246539363cb175b68f239d61aad34f52fdd
Jason Ling <jasonling1996@gmail.com>
2023-01-04 14:33:11 -0800

    Render Data

--- main.js
+++ main.js
@@ -50,3 +50,22 @@ function renderDailyWeather(daily) {
     dailySection.append(element)
   })
 }
+
+const HOUR_FORMATTER = new Intl.DateTimeFormat(undefined, {hour: "numeric"})
+const hourlySection = document.querySelector("[data-hour-section]")
+const hourRowTemplate = document.getElementById("hour-row-template")
+
+function renderHourlyWeather(hourly) {
+  hourlySection.innerHTML = ""
+  hourly.forEach(hour => {
+    const element = hourRowTemplate.contentEditable.cloneNode(true)
+    setValue("temp", hour.temp, {parent: element})
+    setValue("fl-temp", hour.feelsLike, {parent: element})
+    setValue("wind", hour.windSpeed, {parent: element})
+    setValue("precip", hour.precip, {parent: element})
+    setValue("day", DAY_FORMATTER.format(hour.timestamp), {parent: element})
+    setValue("time", HOUR_FORMATTER.format(hour.timestamp), {parent: element})
+    element.querySelector("[data-icon]").src = getIconUrl(hour.iconCode)
+    hourlySection.append(element)
+  })
+}
